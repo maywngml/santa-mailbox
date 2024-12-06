688f70296ca55f2ec707820be70d43af0a5b339e
서주희 <maywngml@naver.com>
2024-12-06 17:47:55 +0900

    [Refactor] 사운드 토글 버튼 메모이제이션 적용 및 코드 스타일 변경

--- components/common/SoundToggleButton.tsx
+++ components/common/SoundToggleButton.tsx
@@ -1,45 +1,47 @@
-import { Fragment, useState } from 'react';
+import { Fragment, memo, useState } from 'react';
 import SpeakerWaveIcon from '@heroicons/react/16/solid/SpeakerWaveIcon';
 import SpeakerXMarkIcon from '@heroicons/react/16/solid/SpeakerXMarkIcon';
 
 interface SoundToggleButtonProps {
-    onSoundOnClick: () => void;
-    onSoundOffClick: () => void;
+  onSoundOnClick: () => void;
+  onSoundOffClick: () => void;
 }
 
-export default function SoundToggleButton({
-    onSoundOnClick,
-    onSoundOffClick,
+function SoundToggleButton({
+  onSoundOnClick,
+  onSoundOffClick,
 }: SoundToggleButtonProps) {
-    const [isMusicOn, setIsMusicOn] = useState<boolean>(false);
+  const [isMusicOn, setIsMusicOn] = useState<boolean>(false);
 
-    const changeIsMusicOn = () => {
-        setIsMusicOn((prevIsMusicOn) => !prevIsMusicOn);
-    };
+  const changeIsMusicOn = () => {
+    setIsMusicOn((prevIsMusicOn) => !prevIsMusicOn);
+  };
 
-    const handleSoundOnClick = () => {
-        changeIsMusicOn();
-        onSoundOnClick();
-    };
+  const handleSoundOnClick = () => {
+    changeIsMusicOn();
+    onSoundOnClick();
+  };
 
-    const handleSoundOffClick = () => {
-        changeIsMusicOn();
-        onSoundOffClick();
-    };
+  const handleSoundOffClick = () => {
+    changeIsMusicOn();
+    onSoundOffClick();
+  };
 
-    return (
-        <Fragment>
-            {isMusicOn ? (
-                <SpeakerWaveIcon
-                    className="absolute w-7 h-7 top-18 right-3 -translate-x-1/2 fill-white z-[1] hover:cursor-pointer lg:w-10 lg:h-10 lg:top-[86px] lg:right-16 2xl:left-[calc(50%+650px)]"
-                    onClick={handleSoundOnClick}
-                />
-            ) : (
-                <SpeakerXMarkIcon
-                    className="absolute w-7 h-7 top-18 right-3 -translate-x-1/2 fill-white z-[1] hover:cursor-pointer lg:w-10 lg:h-10 lg:top-[86px] lg:right-16 2xl:left-[calc(50%+650px)]"
-                    onClick={handleSoundOffClick}
-                />
-            )}
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      {isMusicOn ? (
+        <SpeakerWaveIcon
+          className='absolute w-7 h-7 top-18 right-3 -translate-x-1/2 fill-white z-[1] hover:cursor-pointer lg:w-10 lg:h-10 lg:top-[86px] lg:right-16 2xl:left-[calc(50%+650px)]'
+          onClick={handleSoundOnClick}
+        />
+      ) : (
+        <SpeakerXMarkIcon
+          className='absolute w-7 h-7 top-18 right-3 -translate-x-1/2 fill-white z-[1] hover:cursor-pointer lg:w-10 lg:h-10 lg:top-[86px] lg:right-16 2xl:left-[calc(50%+650px)]'
+          onClick={handleSoundOffClick}
+        />
+      )}
+    </Fragment>
+  );
 }
+
+export default memo(SoundToggleButton);
