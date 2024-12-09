0af2230d7f3a204e5cb478bb009107dc3b74b21d
서주희 <maywngml@naver.com>
2024-12-10 00:34:53 +0900

    [Add] 모달 상태관리 커스텀 훅 추가

---
+++ hooks/useModal.ts
@@ -0,0 +1,39 @@
+import { useCallback, useEffect } from 'react';
+
+interface UseModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function useModal({ isOpen, onClose }: UseModalProps) {
+  const handleEscape = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('keydown', handleEscape);
+    } else {
+      document.removeEventListener('keydown', handleEscape);
+    }
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+    };
+  }, [isOpen, handleEscape]);
+}
