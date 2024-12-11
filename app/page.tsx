094b3c3a32268f9e18ce22fa7574abc67082edb5
서주희 <maywngml@naver.com>
2024-12-12 02:31:30 +0900

    [Add] 기존 카드 영역 컴포넌트로 변경, 편지 전송 상태에 따라 각기 다른 컴포넌트 노출

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
 'use client';
 import { useCallback, useRef, useState } from 'react';
-import Image from 'next/image';
 import { Parisienne } from 'next/font/google';
 import { BackgroundIcons, SoundToggleButton } from '@/components/common';
+import {
+  LetterStatusFailureView,
+  LetterStatusIdleView,
+  LetterStatusLoadingView,
+  LetterStatusSuccessView,
+} from '@/components/home';
 import { LetterFormModal } from '@/components/letter';
 import type { LetterStatusType } from '@/types/letter';
 
 const parisienne = Parisienne({ weight: '400', subsets: ['latin'] });
 
 export default function Home() {
-  const cardRef = useRef<HTMLDivElement | null>(null);
   const soundRef = useRef<HTMLAudioElement | null>(null);
   const [isLetterFormModalOpen, setIsLetterFormModalOpen] =
     useState<boolean>(false);
-  const [letterSendStatus, setLetterSendStatus] =
-    useState<LetterStatusType>('idle');
+  const [letterStatus, setLetterStatus] = useState<LetterStatusType>('idle');
 
   const changeIsLetterFormModalOpen = () => {
     setIsLetterFormModalOpen(
@@ -24,16 +27,6 @@ export default function Home() {
 
   const handleCardClick = () => {
     changeIsLetterFormModalOpen();
-  };
-
-  const handleCardMouseEnter = () => {
-    if (!cardRef.current) return;
-    cardRef.current.style.transform = `scale(1.1)`;
-  };
-
-  const handleCardMouseLeave = () => {
-    if (!cardRef.current) return;
-    cardRef.current.style.transform = `scale(1)`;
   };
 
   const handleSoundOnClick = useCallback(() => {
@@ -52,7 +45,7 @@ export default function Home() {
   }, []);
 
   const handleLetterSend = (status: LetterStatusType) => {
-    setLetterSendStatus(status);
+    setLetterStatus(status);
   };
 
   return (
@@ -76,45 +69,16 @@ export default function Home() {
         />
         현재 브라우저에서는 오디오를 지원하지 않습니다.
       </audio>
-      <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1]'>
-        <div
-          className='relative mx-auto w-[250px] h-[250px] hover:cursor-pointer lg:w-[450px] lg:h-[450px]'
-          ref={cardRef}
-          onClick={handleCardClick}
-          onMouseEnter={handleCardMouseEnter}
-          onMouseLeave={handleCardMouseLeave}
-        >
-          <Image
-            src={'/images/christmas-card.png'}
-            fill
-            sizes='100%'
-            alt='크리스마스 카드 이미지'
-            priority
-          />
+      {
+        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1] break-keep'>
+          {letterStatus === 'failure' && <LetterStatusFailureView />}
+          {letterStatus === 'idle' && (
+            <LetterStatusIdleView onCardClick={handleCardClick} />
+          )}
+          {letterStatus === 'loading' && <LetterStatusLoadingView />}
+          {letterStatus === 'success' && <LetterStatusSuccessView />}
         </div>
-        <p className='mt-6 text-sm lg:text-lg'>
-          편지지를 클릭해 산타할아버지에게 <br className='block lg:hidden' />{' '}
-          편지를 보내보세요 📩
-        </p>
-      </div>
-      <div className='absolute top-[calc(50%-104px)] left-[calc(50%-104px)] -translate-x-1/2 -translate-y-1/2 w-[80px] h-[98px] -rotate-[13.75deg] z-[2] lg:w-[170px] lg:h-[210px] lg:top-[calc(50%-180px)] lg:left-[calc(50%-195px)]'>
-        <Image
-          src={'/images/santa-claus.png'}
-          fill
-          sizes='100%'
-          alt='산타 클로스 이미지'
-          priority
-        ></Image>
-      </div>
-      <div className='absolute top-[calc(50%+50px)] left-[calc(50%+110px)] -translate-x-1/2 -translate-y-1/2 w-[110px] h-[110px] rotate-[31.9deg] lg:w-[300px] lg:h-[300px] lg:top-[calc(50%+50px)] lg:left-[calc(50%+210px)]'>
-        <Image
-          src={'/images/christmas-wreath.png'}
-          fill
-          sizes='100%'
-          alt='크리스마스 화환 이미지'
-          priority
-        ></Image>
-      </div>
+      }
       <BackgroundIcons />
       <LetterFormModal
         isOpen={isLetterFormModalOpen}
