be9489f152f051a85865c8098d8b6f7ea9ca1731
서주희 <maywngml@naver.com>
2024-12-11 21:15:53 +0900

    [Style] 너비, 높이를 100vw, 100vh에서 100%로 변경

--- components/ui/Modal/ModalOverlay.tsx
+++ components/ui/Modal/ModalOverlay.tsx
@@ -5,7 +5,7 @@ interface ModalOverlayProps {
 export default function ModalOverlay({ onClose }: ModalOverlayProps) {
   return (
     <div
-      className='fixed top-0 left-0 w-screen h-screen bg-transparent-black'
+      className='fixed top-0 left-0 w-full h-full bg-transparent-black'
       onClick={onClose}
     />
   );
