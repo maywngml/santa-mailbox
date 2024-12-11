c17199dd828dcda8b3e36f9fdbacb3908e55c8c7
서주희 <maywngml@naver.com>
2024-12-11 17:31:26 +0900

    [Refactor] 모달 컴포넌트들 export 방식 수정

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,1 @@
-export { default as CustomModal } from './Modal/CustomModal';
-export { default as ModalOverlay } from './Modal/ModalOverlay';
-export { default as ModalPortal } from './Modal/ModalPortal';
+export * from './Modal';
