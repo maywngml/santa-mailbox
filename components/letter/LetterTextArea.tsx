5d6facdf367023996d31a9ee47da01b2c2ee3816
서주희 <maywngml@naver.com>
2024-12-11 20:56:17 +0900

    [Update] props에 className 추가, 배경 사이즈 조정

--- components/letter/LetterTextArea.tsx
+++ components/letter/LetterTextArea.tsx
@@ -2,16 +2,18 @@ import { TextareaHTMLAttributes } from 'react';
 
 interface LetterTextAreaProps
   extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  className: string;
   isOutgoing: boolean;
 }
 
 export default function LetterTextArea({
+  className,
   isOutgoing,
   ...props
 }: LetterTextAreaProps) {
   return (
     <textarea
-      className='w-full h-4/5 border-none text-sm leading-[28px] text-dark-brown bg-mobile-lined-paper bg-[size:100%_28px] placeholder-light-brown lg:text-xl boreder-none lg:leading-[40px] lg:bg-pc-lined-paper lg:bg-[size:100%_40px]'
+      className={`${className} w-full h-[90%] border-none text-sm leading-[28px] text-dark-brown bg-mobile-lined-paper bg-[size:100%_28px] bg-local placeholder-light-brown resize-none lg:text-xl boreder-none lg:leading-[40px] lg:bg-pc-lined-paper lg:bg-[size:100%_40px]`}
       placeholder={isOutgoing ? '산타할아버지에게 편지를 작성해보세요' : ''}
       disabled={!isOutgoing}
       {...props}
