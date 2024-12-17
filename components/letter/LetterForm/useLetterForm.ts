d28abc5e612ec1bee70745681637c6afca023b82
서주희 <maywngml@naver.com>
2024-12-18 00:54:27 +0900

    [Update] 1. 반환 데이터에 isLoading 추가 2. 이메일 중복 여부 확인 로직 추가

--- components/letter/LetterForm/useLetterForm.ts
+++ components/letter/LetterForm/useLetterForm.ts
@@ -1,19 +1,28 @@
 import { ChangeEvent, useState, useCallback, FormEvent } from 'react';
+import { useMutation } from '@tanstack/react-query';
 import { validate } from 'email-validator';
 import { useToastMessageContext } from '@/providers/ToastMessageProvider';
+import { getLetter } from '@/api/letter';
 import type { LetterPayload } from '@/types/letter';
 interface UseLetterFormProps {
   onSend: ({ email, content }: LetterPayload) => void;
-  onSuccess?: () => void;
-  onError?: () => void;
 }
 
-export default function useLetterForm({
-  onSend,
-  onSuccess,
-  onError,
-}: UseLetterFormProps) {
+export default function useLetterForm({ onSend }: UseLetterFormProps) {
   const { showToastMessage } = useToastMessageContext();
+  const mutation = useMutation({
+    mutationFn: (email: string) => getLetter(email),
+    onSuccess: (data) => {
+      if (data.hasLetter) {
+        showToastMessage(
+          '이전에 작성한 편지가 있어요. 크리스마스에 메일함을 확인해주세요!'
+        );
+      } else {
+        onSend({ email, content });
+      }
+    },
+    onError: (error) => showToastMessage(error.message),
+  });
   const [email, setEmail] = useState<string>('');
   const [content, setContent] = useState<string>('');
 
@@ -33,13 +42,14 @@ export default function useLetterForm({
     if (!validate(email)) {
       showToastMessage('이메일 주소가 올바르지 않습니다.');
     } else {
-      onSend({ email, content });
+      mutation.mutate(email);
     }
   };
 
   return {
     email,
     content,
+    isLoading: mutation.isPending,
     handleEmailChange,
     handleContentChange,
     handleSubmit,
