7586fdd7e3404fcecf806b184649a90c2bc8d178
서주희 <maywngml@naver.com>
2024-12-18 02:56:37 +0900

    [Add] 로딩 스피너 컴포넌트 구현

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,4 +1,5 @@
 export * from './Modal';
 export { default as Button } from './Button';
 export * from './ToastMessage';
+export { default as LoadingSpinner } from './LoadingSpinner';
 export { default as SoundToggleButton } from './SoundToggleButton';
