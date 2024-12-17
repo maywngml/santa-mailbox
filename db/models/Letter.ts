423c67d9162a129311c8d08a84619ae213c6987d
서주희 <maywngml@naver.com>
2024-12-17 22:20:11 +0900

    [Add] 편지 모델 스키마 정의

---
+++ db/models/Letter.ts
@@ -0,0 +1,44 @@
+import { Schema, model, models } from 'mongoose';
+
+interface Letter {
+  id: string;
+  email: string;
+  content: string;
+  reply: string;
+  createdAt: Date;
+}
+
+const LetterSchema = new Schema<Letter>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    reply: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const LetterModel = models.Letters || model<Letter>('Letters', LetterSchema);
+
+export { LetterModel };
+export type { Letter };
