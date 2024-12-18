a3babb0530af6e74ea6afcd7e96e9000c1ca1627
서주희 <maywngml@naver.com>
2024-12-18 22:32:43 +0900

    [Add] 1. 답장 작성 프롬프트 수정 2. 예약 메일 발송 기능 추가

--- app/api/letter/route.ts
+++ app/api/letter/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import OpenAI from 'openai';
+import Mailchimp from '@mailchimp/mailchimp_transactional';
 import CryptoJS from 'crypto-js';
 import connectDB from '@/db/connectDB';
 import { LetterModel } from '@/db/models/Letter';
+import { AxiosError } from 'axios';
 
 export async function GET(request: NextRequest) {
   try {
@@ -58,37 +60,71 @@ export async function POST(request: NextRequest) {
       apiKey: process.env.OPEN_AI_API_KEY,
     });
     const prompt = `당신은 산타 할아버지입니다. 사람들에게 편지로 답장을 보내고 있습니다. 사용자가 보낸 편지 내용을 바탕으로 답장을 작성해 주세요.
-1. 산타 할아버지의 말투는 따뜻하고 친절하며 긍정적이어야 합니다.  
-2. 답장은 마치 따뜻한 포근한 담요처럼 사용자의 마음을 위로하고 안아주는 느낌이 들어야 합니다. 진심 어린 표현과 부드러운 위로를 담아주세요.  
-3. 사용자가 보낸 편지 내용을 충분히 이해하고, 그에 맞는 **칭찬**, **공감**, **격려**, 또는 **부드러운 조언**을 포함해 주세요.  
-4. 시작은 "안녕, [사용자 이름]야." 또는 "안녕, [사용자 이름]아." 라고 사용자의 이름을 다정하게 부르며 시작해 주세요. 이때, 사용자 이름은 성 빼고 이름만 불러주세요.  
-5. 문장은 마치 오랜 친구처럼 편안하고 친근하게 이야기하듯 작성해 주세요. 너무 형식적이지 않게 자연스러운 일상 대화체로 작성합니다.  
-6. 답장은 사용자의 감정을 공감하는 문장으로 시작해, 위로와 격려를 이어가며 마지막에는 긍정적인 미래를 그리게 하는 따뜻한 메시지로 마무리해 주세요.  
-7. 마무리는 따뜻한 인사말로 끝내 주면서 "2024년 12월 25일 산타 할아버지가🎅"라고 적어주세요. 
-8. 답장은 6~8문장 정도의 길이여야 합니다.
-사용자 이름: "${name}"  
-사용자의 편지 내용: "${content}"  `;
-    const response = await openai.chat.completions.create({
+    1. 산타 할아버지의 말투는 따뜻하고 친절하며 긍정적이어야 합니다.
+    2. 답장은 마치 따뜻한 포근한 담요처럼 사용자의 마음을 위로하고 안아주는 느낌이 들어야 합니다. 진심 어린 표현과 부드러운 위로를 담아주세요.
+    3. 사용자가 보낸 편지 내용을 충분히 이해하고, 그에 맞는 **칭찬**, **공감**, **격려**, 또는 **부드러운 조언**을 포함해 주세요.
+    4. 시작은 "안녕, [사용자 이름]야." 또는 "안녕, [사용자 이름]아." 라고 사용자의 이름을 다정하게 부르며 시작해 주세요. 이때, 사용자 이름은 성 빼고 이름만 불러주세요.
+    5. 문장은 마치 오랜 친구처럼 편안하고 친근하게 이야기하듯 작성해 주세요. 너무 형식적이지 않게 자연스러운 일상 대화체로 작성합니다.
+    6. 답장은 사용자의 감정을 공감하는 문장으로 시작해, 위로와 격려를 이어가며 마지막에는 긍정적인 미래를 그리게 하는 따뜻한 메시지로 마무리해 주세요.
+    7. 마무리는 따뜻한 인사말로 끝내 주면서 "2024년 12월 25일 산타 할아버지가🎅"라고 적어주세요.
+    8. 답장은 6~8문장 정도의 길이여야 합니다.
+    사용자 이름: "${name}"
+    사용자의 편지 내용: "${content}"  `;
+    const gptResponse = await openai.chat.completions.create({
       model: 'gpt-4o-mini',
       messages: [{ role: 'user', content: prompt }],
     });
-    const id = encodeURIComponent(email);
-    const result = await new LetterModel({
+
+    console.log('post letter api gpt', gptResponse);
+
+    const letterId = encodeURIComponent(email);
+    const dbResponse = await new LetterModel({
       ...body,
-      id,
-      reply: response.choices[0].message.content,
+      id: letterId,
+      reply: gptResponse.choices[0].message.content,
       createdAt: new Date(),
     }).save();
 
-    console.log('post letter api', result);
+    console.log('post letter api db', dbResponse);
+
+    const mailchimpTx = Mailchimp(process.env.MAILCHIMP_API_KEY as string);
+    const mailResponse = await mailchimpTx.messages.send({
+      message: {
+        to: [{ email: email, type: 'to' }],
+        html: `<p>드디어 기다리던 크리스마스가 시작됐어요!</p><p>산타 할아버지에게서 특별한 답장이 도착했다는데요!</p><p>지금 바로 확인해보러 가볼까요?</p><a href='https://santa-mailbox.site?letterId=${letterId}'>👉[답장 확인하러 가기]</a><p>산타 할아버지의 마음이 담긴 답장과 함께</p><p>행복이 가득한 크리스마스를 보내시길 바래요.</p><p>올 한해도 고생 많으셨습니다💗 메리 크리스마스!🎄✨</p>`,
+        subject: '산타 할아버지의 답장이 도착했어요💌',
+        from_email: 'admin@santa-mailbox.site',
+        from_name: '산타 우체통📮',
+      },
+      send_at: '2024-12-24 15:00:00',
+    });
+
+    console.log('post letter api mail', mailResponse);
+
+    if (
+      !(mailResponse instanceof AxiosError) &&
+      (mailResponse[0].status === 'invalid' ||
+        mailResponse[0].status === 'rejected')
+    ) {
+      return NextResponse.json(
+        {
+          success: false,
+          error:
+            '편지를 보내던 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+        },
+        { status: 500 }
+      );
+    }
 
     return NextResponse.json({
       success: true,
+      error: null,
     });
   } catch (error) {
     console.error('post letter api error', error);
     return NextResponse.json(
       {
+        success: false,
         error:
           '편지를 보내던 중 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.',
       },
