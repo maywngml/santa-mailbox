ddffa267047e4f548e27eb20dd1660a9386d024c
서주희 <maywngml@naver.com>
2024-12-18 22:47:31 +0900

    [Add] 편지 발송 api 호출하는 함수 추가

--- api/letter.ts
+++ api/letter.ts
@@ -1,5 +1,6 @@
 import fetchAPI from '@/lib/fetchAPI';
 import CryptoJS from 'crypto-js';
+import type { LetterPayload } from '@/types/letter';
 
 export async function getLetter(email: string) {
   const encryptedEmail = CryptoJS.AES.encrypt(
@@ -11,5 +12,17 @@ export async function getLetter(email: string) {
   return fetchAPI({
     method: 'GET',
     url: `/letter?email=${encodedEncryptedEmail}`,
+  });
+}
+
+export async function postLetter({ email, name, content }: LetterPayload) {
+  return fetchAPI({
+    method: 'POST',
+    url: '/letter',
+    body: {
+      email,
+      name,
+      content,
+    },
   });
 }
