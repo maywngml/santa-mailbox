04be56defa584bdf127520f4fd306fac37435cae
서주희 <maywngml@naver.com>
2024-12-19 19:26:24 +0900

    [Style] 상단 마진 제거

--- components/letter/LetterView/LetterView.tsx
+++ components/letter/LetterView/LetterView.tsx
@@ -7,7 +7,7 @@ interface LetterViewProps {
 export default function LetterView({ letter }: LetterViewProps) {
   return (
     <LetterTextArea
-      className='mt-8 lg:mt-6'
+      className='lg:mt-6'
       isOutgoing={false}
       letter={letter}
     />
