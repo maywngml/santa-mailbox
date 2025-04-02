dd7ce518d155cfdc4d40cfa8402316abc05cb67f
서주희 <maywngml@naver.com>
2025-04-02 17:09:55 +0900

    [feat] 메인 페이지 닫음

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 'use client';
 import { useState, useRef } from 'react';
 import { useMutation } from '@tanstack/react-query';
-import {
-  LetterStatusFailureView,
-  LetterStatusIdleView,
-  LetterStatusLoadingView,
-  LetterStatusSuccessView,
-} from '@/components/home';
-import { LetterFormModal } from '@/components/letter';
+import Image from 'next/image';
 import { postLetter } from '@/lib/letter';
 import type { LetterPayload } from '@/types/letter';
 
@@ -42,23 +36,23 @@ export default function Home() {
 
   return (
     <div className='text-center'>
-      {
-        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1] break-keep'>
-          {mutation.isError && (
-            <LetterStatusFailureView onResend={handleLetterResend} />
-          )}
-          {mutation.isIdle && (
-            <LetterStatusIdleView onCardClick={handleCardClick} />
-          )}
-          {mutation.isPending && <LetterStatusLoadingView />}
-          {mutation.isSuccess && <LetterStatusSuccessView />}
+      <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1] break-keep'>
+        <div className='mobile:w-[90vw] mobile:max-w-[400px] flex flex-col items-center gap-3'>
+          <div className='relative mobile:w-[150px] mobile:h-[210px] w-[250px] h-[350px]'>
+            <Image
+              src={'/images/santa-claus.png'}
+              fill
+              sizes='100%'
+              alt='산타 클로스 이미지'
+              priority
+            ></Image>
+          </div>
+          <p className='text-sm lg:text-lg'>
+            우리 내년 크리스마스에 또 만나요!☺️
+            <span className='block'>메리 크리스마스🎄</span>
+          </p>
         </div>
-      }
-      <LetterFormModal
-        isOpen={isLetterFormModalOpen}
-        onClose={changeIsLetterFormModalOpen}
-        onSend={handleLetterSend}
-      />
+      </div>
     </div>
   );
 }
